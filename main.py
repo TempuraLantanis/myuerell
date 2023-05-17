8daf95da2a30057f4b14b23e1171c17d5da55808
TempuraLant <33734673+TempuraLantanis@users.noreply.github.com>
2023-05-17 21:12:39 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,55 @@
+from typing import Union
+from pydantic import BaseModel
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+class BalanceRequest(BaseModel):
+    IBAN:str
+    pin:str
+
+class WithDrawRequest(BaseModel):
+    IBAN:str
+    amount:int
+    
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+
+@app.post("/auth")
+def gibtoken():
+    return {"token":'23314'}
+
+
+@app.post("/balance")
+async def balancePoint(request:BalanceRequest):
+    try:
+        if request.pin== "223":
+            return{"auth":True, "balance":4000}
+        else:
+            return{"auth":False}
+        
+    except:
+        print("error")
+    
+
+
+@app.post("/withdraw")
+async def withdraw(request:WithDrawRequest):
+    try:
+        print("Withdrawing" + request.amount )
+        return {"status":"confirmed"}
+    except:
+        return {"status": "error"}
+
+
+
