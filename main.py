1a5c31540c2313e3222ab4b5603ccef56328be2b
TempuraLant <33734673+TempuraLantanis@users.noreply.github.com>
2023-05-30 14:35:26 +0200

    added Cors

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from typing import Union
 from pydantic import BaseModel
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:8000",
+    "http://localhost:8999",
+    "http://localhost:5173",
+    "0.0.0.0:8999"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class BalanceRequest(BaseModel):
